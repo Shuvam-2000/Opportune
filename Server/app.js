8bd1b522311c99f1aa5f1c8202f5560ecd0bcfb3
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-16 17:48:13 +0530

    Initialize server folder with Nodejs and Expressjs setup,folder structure created for backend

---
+++ Server/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import './utils/connection.js'
+import { configDotenv } from 'dotenv';
+
+
+// intilaize the app
+const app = express();
+
+// Load environment variables
+configDotenv();
+
+// intialize the PORT
+const PORT = process.env.PORT || 4001;
+
+// middlewares
+app.use(express.json());  // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+app.use(cookieParser()); // Enable Cookie Parsing
+app.use(cors({
+    origin: process.env.FRONTEND_URL,  // Frontend URL
+    credentials: true // Allow credentials(cookies)
+})); 
+
+// test route
+app.get('/', (req,res) => {
+    res.send('Hello Server Is Runing');
+});
+
+// Start the Server
+app.listen(PORT, () => console.log(`Server runing on PORT: ${PORT}`));
+
