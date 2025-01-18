bbbcdfa0344c93415aff778778deec7cfecd5f4c
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-18 16:33:09 +0530

    feat: added userprofile update route, formatted the code

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,15 +1,24 @@
-import express from 'express';
-import { newUserRegistration, userLogin, userLogout } from '../Controllers/user.controller.js';
+import express from "express";
+import {
+  newUserRegistration,
+  userLogin,
+  userLogout,
+  userProfileUpdate,
+} from "../Controllers/user.controller.js";
+import { isUserAuthenticated } from "../middlewares/user.middlwares.js";
 
 const router = express.Router();
 
 // user signup route
-router.post('/signup', newUserRegistration);
+router.post("/signup", newUserRegistration);
 
 // user login route
-router.post('/login', userLogin);
+router.post("/login", userLogin);
 
 // user logout route
-router.get('/logout', userLogout);
+router.get("/logout", userLogout);
+
+// user profie update route
+router.put("/profileupdate/:userid", isUserAuthenticated, userProfileUpdate);
 
 export default router;
