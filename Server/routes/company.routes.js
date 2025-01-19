f5bce058b6f6244cca94296f15efff72751e283c
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-19 18:09:45 +0530

    feat: company register route, recuriter registered comapany view route, all company view route & company info view with id route added

---
+++ Server/routes/company.routes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+    getAllcompanies,
+    getcompaniesByID,
+    getRegisteredCompany,
+    registerCompany,
+} from "../Controllers/company.controller.js";
+import { isUserAuthenticated } from "../middlewares/user.middlwares.js";
+import { isUserRecruiter } from "../middlewares/company.middlewares.js";
+
+const router = express.Router();
+
+// register new company route
+router.post("/register", isUserAuthenticated, isUserRecruiter, registerCompany);
+
+// show companies registered by the particular user route
+router.get(
+  "/mycompanies",
+  isUserAuthenticated,
+  isUserRecruiter,
+  getRegisteredCompany
+);
+
+// show all companies to any user 
+router.get('/allcompanies', getAllcompanies);
+
+// show companies with id to auhtenticated user
+router.get("/:id", isUserAuthenticated, getcompaniesByID);
+
+export default router;
