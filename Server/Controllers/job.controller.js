998f21f0c2f824ef0b658af89ec11557d56af2d9
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-24 17:13:13 +0530

    feat: create new jobs, fetch all jobs and fetch any job with keyowrd functionality added

---
+++ Server/Controllers/job.controller.js
@@ -0,0 +1,106 @@
+import JobModel from "../models/job.model.js";
+import CompanyModel from "../models/company.model.js";
+
+// creating new job
+export const createNewJob = async (req,res) => {
+    try {
+        const { jobTitle, 
+            jobDescription, 
+            jobRequirements, 
+            packageOffered, 
+            jobLocation, 
+            jobType, 
+            openPositions,
+            experience } = req.body;
+    
+        // check if all the fields provided to create new job
+        if(!jobTitle || 
+            !jobDescription || 
+            !jobRequirements ||  
+            !jobLocation || 
+            !jobType || 
+            !openPositions ||
+            !experience ) return res.status(404).json({
+                message: 'All Fields Are Required',
+                success: false
+            }); 
+
+        // extracting userID from the middleware
+        const userId = req.user?.id;
+        
+        // extract the company associated with the userId
+        const company = await CompanyModel.findOne({ userId: userId });
+
+        // check if company exists 
+        if(!company) return res.status(404).json({
+            message: 'Compnay Does Not Exists',
+            success: false
+        })
+
+        // extract company id from the database
+        const companyId = company._id;
+
+        // create new job for the said company
+        const createNewJob = await new JobModel({
+            jobTitle, 
+            jobDescription, 
+            jobRequirements, 
+            packageOffered, 
+            jobLocation, 
+            jobType, 
+            openPositions,
+            experience,
+            company: companyId,
+            jobcreated_by: userId
+        });
+
+        // save the new created job to the database
+        createNewJob.save();
+        res.status(201).json({
+            message: `Job created successfully JobTitle: ${jobTitle}`,
+            success: true,
+            createNewJob
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal Server Error',
+            success: false
+        });
+    }
+}
+
+// get all the created jobs
+export const getAllJobs = async (req,res) => {
+    try {
+
+        // fetching jobs with keyword
+        const keyword = req.query.keyword || "";
+        const query = {
+            $or:[
+                {jobTitle:{$regex:keyword, $options: 'i'}},
+                {jobDescription:{$regex:keyword, $options: 'i'}},
+                {jobRequirements:{$regex:keyword, $options: 'i'}}
+            ]
+        }
+
+        // extract all jobs from the database
+        const allJobs = await JobModel.find(query);
+
+        // check if jobs avaliable
+        if(!allJobs) return res.status(404).json({
+            message: 'No Jobs Avaliable Now',
+            success: false
+        })
+
+        res.status(201).json({
+            message: 'Here are the Following Jobs Avaliable Now',
+            success: true,
+            allJobs
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal Server Error',
+            success: false
+        })
+    }
+}
