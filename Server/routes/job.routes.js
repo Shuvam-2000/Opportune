75cb9ebfe6196cf2dc18a3e740f5eebdbdd1965c
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-01-25 17:05:57 +0530

    feat: get all jobs posted, get jobs with id routes added

--- Server/routes/job.routes.js
+++ Server/routes/job.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { isUserAuthenticated } from "../middlewares/user.middlwares.js";
 import { isUserRecruiter } from "../middlewares/company.middlewares.js";
-import { createNewJob, getAllJobs } from "../Controllers/job.controller.js";
+import {
+  createNewJob,
+  getAllJobs,
+  getJobsById,
+} from "../Controllers/job.controller.js";
 
 const router = express.Router();
 
@@ -9,6 +13,9 @@ const router = express.Router();
 router.post("/newjobs", isUserAuthenticated, isUserRecruiter, createNewJob);
 
 // get all jobs route
-router.get('/alljobs', getAllJobs);
+router.get("/alljobs", getAllJobs);
+
+// get jobs with its id route
+router.get("/:id", isUserAuthenticated, getJobsById);
 
 export default router;
