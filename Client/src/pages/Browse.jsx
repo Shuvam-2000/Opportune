dc0581f43b2c642548197bbd640fa7d78812dbac
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-10 17:12:53 +0530

    feat: jobs search page commpleted

--- Client/src/pages/Browse.jsx
+++ Client/src/pages/Browse.jsx
@@ -1,9 +1,60 @@
-
-
 const Browse = () => {
-  return (
-    <div>Browse</div>
-  )
-}
+  const randomjobs = [1, 2, 3, 4, 5, 6];
 
-export default Browse
+  return (
+    <div className="min-h-screen px-4 sm:px-8 py-4">
+      {/* Heading Section */}
+      <div className="flex flex-col items-center justify-center text-sm py-4 sm:my-4 border-gray-300">
+        <h1 className="text-center sm:text-3xl text-xl font-bold">
+          Browsed<span className="text-red-500"> Jobs—</span>
+        </h1>
+      </div>
+
+      {/* Search Results Heading */}
+      <h1 className="text-sm sm:text-2xl font-semibold text-gray-700 mt-4 mb-4">
+        Search Results:{" "}
+        <span className="text-blue-500">({randomjobs.length})</span>
+      </h1>
+
+      {/* Job Cards */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full sm:mt-0 mt-10">
+        {randomjobs.length > 0 ? (
+          randomjobs.map((job) => (
+            <div
+              key={job}
+              className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 hover:bg-gray-50 cursor-pointer sm:p-10 p-6 flex flex-col sm:flex-row items-center gap-6 border border-gray-200"
+            >
+              {/* Job Info */}
+              <div className="flex flex-col flex-grow sm:gap-1 gap-3">
+                <h1 className="text-lg mr-8 font-bold text-gray-900">
+                  Company Name
+                </h1>
+                <p className="text-sm text-gray-700 font-medium mt-2 mb-4">
+                  Job Title
+                </p>
+                <p className="text-sm text-gray-600">
+                  Short job description goes here.
+                </p>
+
+                <div className="flex justify-between text-sm text-gray-500 font-medium mt-8 sm:gap-4 gap-4">
+                  <span>Type</span>
+                  <span>Package</span>
+                  <span>Experience</span>
+                  <span>Positions</span>
+                </div>
+                {/* Apply Button */}
+                <button className="text-sm bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg mt-6 sm:px-2 sm:py-2 px-20 py-2 transition-all duration-300 cursor-pointer">
+                  Apply
+                </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-gray-500">No jobs searched.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Browse;
