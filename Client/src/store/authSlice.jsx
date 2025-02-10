001e7d7c2cbc9fb6f04c417f1e7182abb0857252
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-10 17:14:23 +0530

    feat: reducers added for handling user status

--- Client/src/store/authSlice.jsx
+++ Client/src/store/authSlice.jsx
@@ -3,15 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name: "auth",
     initialState: {
-        loading: false
+        loading: false,
+        user: null
     },
     reducers:{
         // actions
         setlodaing:(state, action) => {
             state.loading = action.payload  // set loading to true
+        },
+        setUser:(state, action) => {
+            state.user = action.payload  // set user to data
         }
     }
 })
 
-export const { setlodaing } = authSlice.actions;
+export const { setlodaing, setUser } = authSlice.actions;
 export default authSlice.reducer;
