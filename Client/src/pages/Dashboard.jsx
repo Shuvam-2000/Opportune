5166b899822ae6d1f45c9185617436e1ee44f683
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-10 19:10:51 +0530

    feat: user dashboard heading stying done, functionality added to show user fullname dynamically with redux

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
-
+import Profile from "./Profile";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const { user } = useSelector((store) => store.auth);
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-between text-sm py-4 border-b border-b-gray-300 relative">
+        {/* Username */}
+        <h1 className="sm:text-3xl text-2xl font-bold text-black cursor-pointer">
+          Hi,
+          <span className="sm:text-3xl text-2xl font-bold text-red-600">
+            { user ? user.fullname : null }
+          </span>
+        </h1>
+      </div>
+      <Profile />
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
