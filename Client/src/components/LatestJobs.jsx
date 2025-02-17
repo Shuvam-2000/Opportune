eebe90f4ec73413898d7df105b2723082e03e54d
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-17 18:12:40 +0530

    feat: functioanlity to show posted jobs added - added fucntioanlity to fetch and show latest jobs from the redux store

--- Client/src/components/LatestJobs.jsx
+++ Client/src/components/LatestJobs.jsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const LatestJobs = () => {
-  const jobs = [1, 2, 3, 4, 5, 6, 7, 8];
-  const navigate = useNavigate()
+  const { alljobs } = useSelector((store) => store.job);
+  const navigate = useNavigate();
 
   return (
     <div className="mt-8 px-4">
@@ -10,45 +11,55 @@ const LatestJobs = () => {
         Latest <span className="text-red-500">Job Openings—</span>
       </h1>
       <p className="text-center text-gray-500 sm:text-sm text-xs mb-8">
-          Discover exciting & latest job opportunities that match your skills 
-        </p>
+        Discover exciting & latest job opportunities that match your skills
+      </p>
 
       {/* Job Postings */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {jobs.slice(0, 6).map((job) => (
-          <div
-            key={job}
-            className=" rounded-lg shadow-md hover:shadow-2xl transition-all duration-300 hover:bg-gray-50 cursor-pointer p-6 flex flex-col items-center text-center py-10"
-          >
-            <p className="text-lg font-semibold text-gray-700">Job #{job}</p>
+        {alljobs?.length > 0 ? (
+          alljobs.slice(0, 6).map((job) => (
+            <div
+              key={job._id}
+              className="rounded-lg shadow-md hover:shadow-2xl transition-all duration-300 hover:bg-gray-50 cursor-pointer p-8 flex flex-col items-center text-center py-12"
+            >
+              <p className="text-lg font-bold mb-2 text-gray-900">
+                {job.company?.companyName}
+              </p>
 
-            <div className="mt-4 w-full">
-              <p className="text-sm font-bold mb-2 text-gray-900">
-                Company Name
-              </p>
-              <p className="text-xs text-gray-500 font-medium mb-4">
-                Short job description goes here.
-              </p>
-              <p className="text-xs text-gray-500 font-medium mb-4">
-                Job Title
-              </p>
-              <div className="flex gap-4 justify-center">
-                <p className="text-xs text-gray-500 font-medium mb-4">
-                  Job type
+              <div className="mt-6 py-2 w-full">
+                <p className="text-sm font-semibold text-black sm:mb-6 mb-4">
+                  {job.jobTitle}
                 </p>
-                <p className="text-xs text-gray-500 font-medium mb-4">
-                  Package
+                <p className="text-xs text-gray-500 font-medium sm:mb-6 mb-4">
+                  {job.jobDescription}
                 </p>
-                <p className="text-xs text-gray-500 font-medium mb-4">
-                  Experience
-                </p>
+
+                <div className="flex gap-4 justify-center">
+                  <p className="text-xs text-black font-medium mb-4">
+                    Type: {job.jobType || ""}
+                  </p>
+                  <p className="text-xs text-black font-medium mb-4">
+                    Salary: {job.packageOffered || "Not Disclosed"}
+                  </p>
+                  <p className="text-xs text-black font-medium mb-4">
+                    Exp: {job.experience}
+                  </p>
+                </div>
+
+                <button
+                  onClick={() => navigate(`/jobdescription/${job._id}`)}
+                  className="w-full text-sm bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg py-2 transition-all duration-300 cursor-pointer"
+                >
+                  Check Details
+                </button>
               </div>
-              <button onClick={() => navigate('/jobdescription/:id')} className="w-full text-sm bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg py-2 transition-all duration-300 cursor-pointer">
-                Apply
-              </button>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p className="text-center text-gray-500">
+            No job openings available at the moment.
+          </p>
+        )}
       </div>
     </div>
   );
