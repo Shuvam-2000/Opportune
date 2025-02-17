58e96b72f398725dd6dad8fd206fea49815ad159
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-17 18:16:57 +0530

    feat: custom job hook added to fetch latest jobs from backend stored in redux

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,15 +1,17 @@
-import Banner from "../components/Banner"
-import JobCategory from "../components/JobCategory"
-import LatestJobs from "../components/LatestJobs"
+import Banner from "../components/Banner";
+import JobCategory from "../components/JobCategory";
+import LatestJobs from "../components/LatestJobs";
+import useGetAllJobs from "../hooks/useGetAllJobs";
 
 const Home = () => {
+  useGetAllJobs();
   return (
-    <>  
+    <>
       <Banner />
       <JobCategory />
       <LatestJobs />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
