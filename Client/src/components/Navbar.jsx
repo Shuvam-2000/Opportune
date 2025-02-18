827ae942f28de01ef565c2d66836362d506575b8
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-18 18:07:24 +0530

    feat: condtional rendering added for recuriter & candidate login

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -46,18 +46,33 @@ const Navbar = () => {
 
       {/* Navigation Links */}
       <ul className="hidden md:flex justify-evenly mt-2 gap-5 font-medium">
-        <NavLink to="/" className="group">
-          <li className="py-1">HOME</li>
-          <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
-        </NavLink>
-        <NavLink to="/explorejobs" className="group">
-          <li className="py-1">EXPLORE JOBS</li>
-          <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
-        </NavLink>
-        <NavLink to="/browse" className="group">
-          <li className="py-1">BROWSE</li>
-          <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
-        </NavLink>
+        {user && user.role === "recruiter" ? (
+          <>
+            <NavLink to="/admin/companies" className="group">
+              <li className="py-1">COMPANIES</li>
+              <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
+            </NavLink>
+            <NavLink to="/admin/jobs" className="group">
+              <li className="py-1">JOBS POSTED</li>
+              <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink to="/" className="group">
+              <li className="py-1">HOME</li>
+              <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
+            </NavLink>
+            <NavLink to="/explorejobs" className="group">
+              <li className="py-1">EXPLORE JOBS</li>
+              <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
+            </NavLink>
+            <NavLink to="/browse" className="group">
+              <li className="py-1">BROWSE</li>
+              <hr className="border-none outline-none h-0.5 bg-red-600 w-3/5 m-auto hidden group-hover:block" />
+            </NavLink>
+          </>
+        )}
       </ul>
 
       {/* Right Section */}
@@ -71,7 +86,7 @@ const Navbar = () => {
               user?.profile?.profilePicture
                 ? "sm:w-10 w-8 h-8 sm:h-10 rounded-full object-cover border border-gray-300 cursor-pointer"
                 : "sm:w-6 w-4 cursor-pointer"
-            }                                                 
+            }
             onClick={() => setProfileMenuVisible(!profileMenuVisible)}
           />
         ) : (
@@ -85,15 +100,17 @@ const Navbar = () => {
 
         {/* Profile Dropdown Menu */}
         {profileMenuVisible && (
-          <div className="absolute right-0 mt-34 py-2 bg-white shadow-lg rounded-lg w-40 z-10">
+          <div className={`absolute right-0 ${user && user.role === "recruiter" ? 'mt-20' : 'mt-34'} py-2 bg-white shadow-lg rounded-lg w-40 z-10`}>
             <ul className="flex flex-col text-gray-700">
-              <NavLink
-                to="/dashboard"
-                className="px-4 py-2 hover:bg-gray-100 hover:text-red-500"
-                onClick={() => setProfileMenuVisible(false)}
-              >
-                Your Dashboard
-              </NavLink>
+              {user && user.role !== "recruiter" && (
+                <NavLink
+                  to="/dashboard"
+                  className="px-4 py-2 hover:bg-gray-100 hover:text-red-500"
+                  onClick={() => setProfileMenuVisible(false)}
+                >
+                  Your Dashboard
+                </NavLink>
+              )}
               <NavLink
                 to="/"
                 className="px-4 py-2 hover:bg-gray-100 hover:text-red-500"
@@ -118,7 +135,7 @@ const Navbar = () => {
 
       {/* Sidebar for Mobile View */}
       <div
-        className={`fixed top-0 right-0 h-95 bg-white rounded-b-lg shadow-lg transform transition-transform duration-500 ease-in-out ${
+        className={`fixed top-0 right-0 h-90 bg-white rounded-b-lg shadow-lg transform transition-transform duration-500 ease-in-out ${
           menuVisible ? "translate-y-0" : "-translate-y-full"
         }`}
         style={{ width: "100%" }}
@@ -132,30 +149,50 @@ const Navbar = () => {
             &times;
           </button>
           {/* Sidebar Links */}
-          <NavLink
-            className="py-2 pl-6 text-center font-medium"
-            to="/"
-            onClick={() => setMenuVisible(false)}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className="py-2 pl-6 text-center font-medium"
-            to="/explorejobs"
-            onClick={() => setMenuVisible(false)}
-          >
-            Explore Jobs
-          </NavLink>
-          <NavLink
-            className="py-2 pl-6 text-center font-medium"
-            to="/browse"
-            onClick={() => setMenuVisible(false)}
-          >
-            Browse
-          </NavLink>
-          {user ? (
-            ""
+          {user && user.role === "recruiter" ? (
+            <>
+              <NavLink
+                className="py-2 pl-6 text-center font-medium"
+                to="/admin/companies"
+                onClick={() => setMenuVisible(false)}
+              >
+                Companies
+              </NavLink>
+              <NavLink
+                className="py-2 pl-6 text-center font-medium"
+                to="/admin/jobs"
+                onClick={() => setMenuVisible(false)}
+              >
+                Jobs Posted
+              </NavLink>
+            </>
           ) : (
+            <>
+              <NavLink
+                className="py-2 pl-6 text-center font-medium"
+                to="/"
+                onClick={() => setMenuVisible(false)}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className="py-2 pl-6 text-center font-medium"
+                to="/explorejobs"
+                onClick={() => setMenuVisible(false)}
+              >
+                Explore Jobs
+              </NavLink>
+              <NavLink
+                className="py-2 pl-6 text-center font-medium"
+                to="/browse"
+                onClick={() => setMenuVisible(false)}
+              >
+                Browse
+              </NavLink>
+            </>
+          )}
+          {/* Show Login link if no user is logged in */}
+          {!user && (
             <NavLink
               className="py-2 pl-6 text-center font-medium"
               to="/login"
