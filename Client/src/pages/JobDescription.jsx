1f103b0e7385dcec81e4b78f9b29d22da03ebb4f
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-18 18:11:15 +0530

    feat: api integration done - apply to job feature added - fucntioanlity added to dynamicaly disable and enable the apply button accoridng to the suer job apply status

--- Client/src/pages/JobDescription.jsx
+++ Client/src/pages/JobDescription.jsx
@@ -1,20 +1,59 @@
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { setSingleJob } from "../store/jobSlice";
+import toast from "react-hot-toast";
 
 const JobDescription = () => {
   const params = useParams();
   const jobId = params.id;
   const dispatch = useDispatch();
-  const isApplied = false;
+
+  // state for handling error messages if job info not avaliable
+  const [errorMessage, setErrorMessage] = useState("");
 
   // fetching user from redux sotre
   const { user } = useSelector((store) => store.auth);
 
   // fetching single job info from the redux store
   const { singlejob } = useSelector((store) => store.job);
+
+  // fucntionality to show job apply state for every user
+  const isApplied =
+    singlejob?.applications?.some(
+      (application) => application.applicant === user?._id
+    ) || false;
+
+  // state to handlejob apply state for user dynamically
+  const [alreadyApplied, setAlreadyApllied] = useState(isApplied);
+
+  // fucntionality to apply in a job
+  const applyToJob = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:4000/application/apply/${jobId}`,
+        { withCredentials: true }
+      );
+
+      if (res.data.success) {
+        setAlreadyApllied(true);
+        // Update Redux store to reflect application state(for real time UI update)
+        dispatch(
+          setSingleJob({
+            ...singlejob,
+            applications: [
+              ...(singlejob?.applications || []),
+              { applicant: user?._id },
+            ],
+          })
+        );
+        toast.success(res.data.message || "Applied to the Job Successfully");
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong");
+    }
+  };
 
   // functionality to fetch job information with its id
   useEffect(() => {
@@ -25,13 +64,27 @@ const JobDescription = () => {
         });
         if (res.data?.success) {
           dispatch(setSingleJob(res.data.job));
+          setAlreadyApllied(
+            res?.data?.job?.applications.some(
+              (application) => application.applicant === user?._id 
+            ) || false
+          );
         }
       } catch (error) {
-        console.error(error);
+        setErrorMessage(error.res?.data?.message || "Something Went Wrong");
       }
     };
     getSingleAllJobs();
   }, [jobId, dispatch, user?._id]);
+
+  // show error message in case job information not avaliable
+  if (errorMessage) {
+    return (
+      <p className="text-center text-gray-700 mt-50 text-lg font-bold">
+        {errorMessage}
+      </p>
+    );
+  }
 
   return (
     <div className="mt-4 p-6 bg-white shadow-lg rounded-lg max-w-8xl mx-auto">
@@ -93,14 +146,15 @@ const JobDescription = () => {
 
       <div className="mt-6 text-left">
         <button
+          onClick={alreadyApplied ? null : applyToJob}
           className={`sm:px-6 sm:py-2 px-4 py-2 text-white font-medium rounded-lg ${
-            isApplied
+            alreadyApplied
               ? "bg-red-600 cursor-not-allowed opacity-60"
               : "bg-red-500 hover:bg-red-600 cursor-pointer"
           }`}
-          disabled={isApplied}
+          disabled={alreadyApplied}
         >
-          {isApplied ? "Already Applied" : "Apply Now"}
+          {alreadyApplied ? "Already Applied" : "Apply Now"}
         </button>
       </div>
     </div>
