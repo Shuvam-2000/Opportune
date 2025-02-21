fd46cfc9778f509d3b0e8b991fb0927f024abc75
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-21 17:29:54 +0530

    feat: api integration done for fetching company info wih Id

---
+++ Client/src/hooks/useGetCompanyById.jsx
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { setSingleCompany } from "../store/companySlice"
+
+const useGetCompanyById = (companyId) => {
+  const dispatch = useDispatch()
+  const [errorMessage, setErrorMessage] = useState('')
+  useEffect(() => {
+    const fetchSingleCompanyData = async () => {
+        try {
+            const res = await axios.get(`http://localhost:4000/company/${companyId}`,{withCredentials: true})
+            if(res?.data?.success){
+                dispatch(setSingleCompany(res?.data?.company))
+            }
+        } catch (error) {
+            setErrorMessage(error.response?.data?.message || 'Error Fetching Company Info')
+        }
+    }
+    fetchSingleCompanyData();
+  },[dispatch, companyId])
+  return { errorMessage };
+}
+
+export default useGetCompanyById;
