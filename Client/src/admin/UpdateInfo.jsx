33f774a50e8173c024159716cc7c488b91ce0f7f
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-21 17:25:47 +0530

    feat: API integration done for upating the company info

--- Client/src/admin/UpdateInfo.jsx
+++ Client/src/admin/UpdateInfo.jsx
@@ -1,18 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import axios from "axios";
+import useGetCompanyById from "../hooks/useGetCompanyById";
 
 const UpdateInfo = () => {
-  const navigate = useNavigate()
-  const [updateCompanyInfo, setUpdateCompanyInfo] = useState({});
+  const params = useParams();
+  const navigate = useNavigate();
+  useGetCompanyById(params.id)
+  
+  // state to handle data from the fields for company info update
+  const [updateCompanyInfo, setUpdateCompanyInfo] = useState({
+    companyName: "",
+    companyLocation: "",
+    description: "",
+    companyWebsite: "",
+    logo: null,
+  });
+
+  // fetching single particular company info from redux
+  const { singleCompany } = useSelector((store) => store.company);
+
+  // implementing the onchange function for file uploads
+  const fileHandler = (e) => {
+    const file = e.target.files?.[0];
+    setUpdateCompanyInfo({ ...updateCompanyInfo, file });
+  };
 
   // implementing the onchange function for other fields uploads
-  const fileHandler = (e) => {};
+  const updateHandler = (e) => {
+    setUpdateCompanyInfo({
+      ...updateCompanyInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const updateHandler = (e) => {};
+  // funcitonality to update the company information
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("companyName", updateCompanyInfo.companyName);
+    formData.append("companyLocation", updateCompanyInfo.companyLocation);
+    formData.append("description", updateCompanyInfo.description);
+    formData.append("companyWebsite", updateCompanyInfo.companyWebsite);
+    if (updateCompanyInfo.file) {
+      formData.append("logo", updateCompanyInfo.file);
+    }
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/company/companyInfo/${params.id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res?.data?.success) {
+        navigate("/admin/companies");
+        toast.success(res?.data?.message || "Company Info Updated Sucessfully");
+      }
+    } catch (error) {
+      toast.error(error?.res?.message || "Error Updating Company Info");
+    }
+  };
 
-  const handleSubmit = () => {};
+  // fetching the company data from redux store
+  useEffect(() => {
+    setUpdateCompanyInfo({
+      companyName: singleCompany?.companyName || "",
+      companyLocation: singleCompany?.companyLocation || "",
+      description: singleCompany?.description || "",
+      companyWebsite: singleCompany?.companyWebsite || "",
+      logo: singleCompany?.logo || null,
+    });
+  }, [singleCompany]);
 
   return (
     <>
@@ -44,7 +110,7 @@ const UpdateInfo = () => {
             onChange={updateHandler}
             value={updateCompanyInfo.description}
             className="border sm:p-3 p-2 text-sm w-full rounded hover:border-black"
-            placeholder="Something About your Company"
+            placeholder="Something About Your Company"
           ></textarea>
           <input
             type="text"
@@ -52,7 +118,7 @@ const UpdateInfo = () => {
             onChange={updateHandler}
             value={updateCompanyInfo.companyWebsite}
             className="border sm:p-3 p-2 text-sm w-full rounded hover:border-black"
-            placeholder="example@gmail.com (Add Your Company Website)"
+            placeholder="www.example.com (Add Your Company Website)"
           />
 
           {/* Add Company Logo */}
@@ -66,7 +132,8 @@ const UpdateInfo = () => {
 
           {/* Buttons side by side with proper spacing */}
           <div className="flex justify-center gap-4 mt-4">
-            <button onClick={() => navigate('/admin/companies')}
+            <button
+              onClick={() => navigate("/admin/companies")}
               type="button"
               className="bg-gray-300 text-gray-700 sm:px-6 sm:py-2 px-3 py-2 rounded-md hover:bg-gray-400 transition-all duration-300 cursor-pointer"
             >
@@ -76,7 +143,7 @@ const UpdateInfo = () => {
               type="submit"
               className="bg-red-500 text-white sm:px-4 sm:py-2 px-3 py-2 rounded-md hover:bg-red-600 transition-all duration-300 cursor-pointer"
             >
-              Update 
+              Update
             </button>
           </div>
         </form>
