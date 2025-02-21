26c0982199c5273f7d8f455349219178717e4824
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-21 17:28:31 +0530

    feat: API integration done to fetch data of comanis registered by any user

---
+++ Client/src/hooks/useGetAllCompanies.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setAllCompany } from "../store/companySlice";
+
+const useGetAllCompanies = () => {
+    const dispatch = useDispatch();
+    const [errorMessage, setErrorMessage] = useState('')
+    useEffect(() => {
+        const getAllCompaniesData = async () => {
+            try {
+                const res = await axios.get(`http://localhost:4000/company/mycompanies`,{withCredentials: true})
+                if(res?.data?.success){
+                    dispatch(setAllCompany(res?.data?.company))
+                }
+            } catch (error) {
+                setErrorMessage(error.response?.data?.message || "Error Fetching Companies")
+            }
+        }
+        getAllCompaniesData()
+    },[])
+    return { errorMessage }
+};
+
+export default useGetAllCompanies;
