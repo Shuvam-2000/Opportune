7d3f0c89f6640c5ed2e90005ae121be00bf8c5e3
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-21 17:26:52 +0530

    feat: reducers added for stroing single compnay data and all the companies data registeed by any user

--- Client/src/store/companySlice.jsx
+++ Client/src/store/companySlice.jsx
@@ -3,15 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const companySlice = createSlice({
     name: "company",
     initialState: {
-        singleCompany: null  // set single company to null by default
+        singleCompany: null,  // set single company to null by default
+        allcompanies: []  // set all companies data to empty array by default
     },
     reducers:{
         // actions
         setSingleCompany:(state,action) => {
             state.singleCompany = action.payload  // set single company to data
+        },
+        setAllCompany:(state,action) => {
+            state.allcompanies = action.payload  // set all companies to data
         }
     }
 });
 
-export const { setSingleCompany } = companySlice.actions;
+export const { setSingleCompany, setAllCompany } = companySlice.actions;
 export default companySlice.reducer;
