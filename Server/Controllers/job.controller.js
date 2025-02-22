6d7ea97d10beb3402c6df7efe4c18ae763d926be
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-22 17:35:07 +0530

    chore: commented the unnessecary code

--- Server/Controllers/job.controller.js
+++ Server/Controllers/job.controller.js
@@ -142,3 +142,31 @@ export const getJobsById = async (req,res) => {
         })
     }
 }
+
+// export const fetchJobsCreated = async (req, res) => {
+//     try {
+//         const userId = req.user?.id;
+
+//         if (!userId) {
+//             return res.status(404).json({
+//             message: "User ID not found", 
+//             success: false 
+//         })}
+
+//         const createdJobs = await JobModel.find({ jobcreated_by: userId });
+
+//         if (createdJobs.length === 0) {
+//             return res.status(404).json({ 
+//             message: "No Jobs Found", 
+//             success: false 
+//         })}
+
+//         res.status(200).json({ 
+//             message: "Jobs Available", 
+//             success: true, 
+//             jobs: createdJobs 
+//         });
+//     } catch (error) {
+//         res.status(500).json({ message: "Internal Server Error", success: false });
+//     }
+// };
