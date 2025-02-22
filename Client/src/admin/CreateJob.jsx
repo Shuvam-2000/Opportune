ec61ae7e4b112e32a2b8e916623125c53d3f020f
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-22 17:36:15 +0530

    applicant and create job page

---
+++ Client/src/admin/CreateJob.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const CreateJob = () => {
+  return (
+    <>
+      <Navbar />
+        <div>Create Job</div>
+      <Footer />
+    </>
+  );
+};
+
+export default CreateJob;
