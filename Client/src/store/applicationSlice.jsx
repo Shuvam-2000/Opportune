fa77eb4a8cce0ace6c1e51284cc8f86d4ceb8b3e
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-26 17:48:48 +0530

    reducer added to store application data and update application status

--- Client/src/store/applicationSlice.jsx
+++ Client/src/store/applicationSlice.jsx
@@ -3,15 +3,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const applicationSlice = createSlice({
     name: "application",
     initialState: {
-        applicants: [] // Set applicants to empty array
+        applicants: null // Set applicants to null
     },
     reducers: {
         // actions
         setApplicants: (state, action) => {
-            state.applicants = action.payload; // Set applicants to dta
+            state.applicants = action.payload; // Set applicants to data
+        },
+        resetApplicants: (state) => {
+            state.applicants = null; // Reset data to null when changing jobs
+        },
+        updateStatus: (state, action) => {  // update status of job application
+            const { id, status } = action.payload;
+            state.applicants.applications = state.applicants.applications.map((applicant) =>  
+                applicant._id === id ? { ...applicant, applicationStatus: status } : applicant
+            );
         }
     }
 });
 
-export const { setApplicants } = applicationSlice.actions;
+export const { setApplicants, resetApplicants, updateStatus  } = applicationSlice.actions;
 export default applicationSlice.reducer;
