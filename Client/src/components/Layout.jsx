3feba6adcd01436ef74001273a1bc7766ed552d4
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-02-27 16:51:15 +0530

    feat: chatbot component added to the layout

--- Client/src/components/Layout.jsx
+++ Client/src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import Chatbot from "./Chatbot";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
         <Outlet />
+        <Chatbot />
       <Footer />
     </>
   );
